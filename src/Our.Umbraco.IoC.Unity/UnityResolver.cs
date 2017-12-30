bd3808dfdc2c9ba3529850108ad223775dc6e095
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-12-30 14:08:06 +0100

    Unity support, first draft

---
+++ src/Our.Umbraco.IoC.Unity/UnityResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity;
+
+namespace Our.Umbraco.IoC.Unity
+{
+    internal class UnityResolver : Resolver
+    {
+        private IUnityContainer _container;
+        public UnityResolver WithContext(IUnityContainer container)
+        {
+            if (container == null) throw new ArgumentNullException(nameof(container));
+            _container = container;
+            return this;
+        }
+        public override TService Resolve<TService>()
+        {
+            return _container.Resolve<TService>();
+        }
+    }
+}
